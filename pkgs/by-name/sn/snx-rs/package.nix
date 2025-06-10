38bb7430087c32150d41a251a3d95336a073b19c
Jonathan Thiessen <jonathan@tangential.ca>
2025-06-10 09:55:59 -0700

    snx-rs: 3.1.2 -> 4.4.1

--- pkgs/by-name/sn/snx-rs/package.nix
+++ pkgs/by-name/sn/snx-rs/package.nix
@@ -1,63 +1,65 @@
 {
   fetchFromGitHub,
-  glib,
-  gtk3,
-  iproute2,
-  kdePackages,
+  # glib,
+  # gtk3,
+  gtk4,
+  # iproute2,
+  # kdePackages,
   lib,
-  libappindicator,
-  libappindicator-gtk2,
-  libappindicator-gtk3,
-  libayatana-appindicator,
-  libsoup_3,
+  # libappindicator,
+  # libappindicator-gtk2,
+  # libappindicator-gtk3,
+  # libayatana-appindicator,
+  # libsoup_3,
   openssl,
   pkg-config,
   rustPlatform,
-  webkitgtk_4_1,
+  # webkitgtk_4_1,
   nix-update-script,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "snx-rs";
-  version = "3.1.2";
+  version = "4.4.1";
 
   src = fetchFromGitHub {
     owner = "ancwrd1";
     repo = "snx-rs";
     tag = "v${version}";
-    hash = "sha256-bLuIXd2pqqiyEP+lDTJYVDZkRZ0HcDkKFZd/qlpuf98=";
+    hash = "sha256-Juv38ALXf1nMeokBH7Z+39oIscXW7S+OxdD/ZSNs49U=";
   };
 
   passthru.updateScript = nix-update-script { };
 
   nativeBuildInputs = [
-    iproute2
+    # iproute2
     pkg-config
   ];
 
   buildInputs = [
-    glib
-    gtk3
-    kdePackages.kstatusnotifieritem
-    libappindicator
-    libappindicator-gtk2
-    libappindicator-gtk3
-    libayatana-appindicator
-    libsoup_3
+    # glib
+    # gtk3
+    gtk4
+    # kdePackages.kstatusnotifieritem
+    # libappindicator
+    # libappindicator-gtk2
+    # libappindicator-gtk3
+    # libayatana-appindicator
+    # libsoup_3
     openssl
-    webkitgtk_4_1
+    # webkitgtk_4_1
   ];
 
-  postPatch = ''
-    substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
-      --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
-  '';
+  # postPatch = ''
+  #   substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
+  #     --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
+  # '';
 
   checkFlags = [
     "--skip=platform::linux::net::tests::test_default_ip"
   ];
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-E5OJVf9CkLn5mFtk4Yacs2OIvAuIw0idSs7QuTNvfgU=";
+  cargoHash = "sha256-NcoTdu/CQRu0RuZjlngP8lTPaomEiPTcfn2hAt+YjwA=";
 
   meta = {
     description = "Open source Linux client for Checkpoint VPN tunnels";
